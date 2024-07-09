fcb68cd6956a5eb0c7b5d3625be4d3a4becd36f8
75098360 <lancy.gao1997@gmail.com>
2024-07-09 21:30:52 +1000

    update package, runned npm install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-xl text-red-400">Hey it's workig</div>;
+  return <div className="text-xl text-red-400 font-light">Hey it's working</div>;
 };
 
 export default App;
