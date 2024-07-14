6d0ca600fd117d858df4ae8971f5ebf6aee05eee
Linfei Fu <linfei.fu.2803@gmail.com>
2024-07-14 21:59:34 +1000

    UPDATE-Feature-signup-page-updated background shadow as Mary suggested

--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -6,10 +6,12 @@ import Policy from "./components/Policy";
 const SignupPage: React.FC = () => {
   return (
     <div className="flex h-screen w-screen bg-hero-pattern bg-cover relative overflow-hidden">
-      <div className="h-screen w-screen bg-gradient-to-tl from-[#48321E00] to-[#000000]">
-        <WelcomeMessage />
-        <ContactForm />
-        <Policy />
+      <div className="h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
+        <div className="absolute h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
+          <WelcomeMessage />
+          <ContactForm />
+          <Policy />
+        </div>
       </div>
     </div>
   );
