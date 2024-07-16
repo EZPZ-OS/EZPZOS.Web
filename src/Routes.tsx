4e68895401227b8dc9aeb2c037ff3f71cb9d2850
LeoShang <0415150730sang@gmail.com>
2024-07-17 09:09:40 +1000

    Add OptPage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import QRScannerPage from "./Pages/QR/QRScannerPage";
 import Menu from "./Pages/Menu/Menu";
-import SignupPage from "./Pages/SignUp/SignUpPage";
+import SignupPage from "./Pages/signup/SignupPage";
+import OptPage from "./Pages/OptPage/OptPage";
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="signup" element={<SignupPage />} />
-    <Route path="scan" element={<QRScannerPage />} />
     <Route path="menu" element={<Menu />} />
+      <Route path="opt" element={<OptPage />} />
   </Routes>
 );
 
