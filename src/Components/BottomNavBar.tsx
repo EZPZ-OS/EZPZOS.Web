ac1b5a2e82621f797c9f0b4b7610e0b7a7757ea1
danxuanw <w.dan.xuan@gmail.com>
2024-07-16 15:09:04 +1000

    add qr validation, add /menu diaplay table number

--- src/Components/BottomNavBar.tsx
+++ src/Components/BottomNavBar.tsx
@@ -4,7 +4,7 @@ import { FaUser } from "react-icons/fa6";
 
 const BottomNavBar = () => {
   return (
-    <div className="bg-[#D9D9D9] p-4 text-[#988B8B] text-center flex">
+    <div className="bg-[#D9D9D9] p-4 text-[#988B8B] text-center flex bottom-0">
       <p className="text-center text-xl">
         **THIS IS BOTTOM.NAV.BAR's PLACE HOLDER ONLY**
       </p>
--- src/pages/QR/QRScannerPage.tsx
+++ src/pages/QR/QRScannerPage.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useEffect, useState } from "react";
 import jsQR from "jsqr";
 import { useNavigate } from "react-router-dom";
 import BottomNavBar from "../../Components/BottomNavBar";
-import { IoChevronBackOutline } from "react-icons/io5";
 import { IoChevronBackCircleSharp } from "react-icons/io5";
 import ScanIcon from "../../Assets/Icons/ScanIcon.png";
 
@@ -10,6 +9,7 @@ const QRScannerPage: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [scanning, setScanning] = useState(true);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -55,7 +55,17 @@ const QRScannerPage: React.FC = () => {
           setScanning(false);
           const params = new URLSearchParams(code.data);
           const tableNumber = params.get("tableNumber");
-          navigate(`/menu?tableNumber=${tableNumber}`);
+          if (tableNumber) {
+            setTimeout(() => {
+              navigate(`/menu?tableNumber=${tableNumber}`);
+            }, 2000); // Add a 2-second delay before navigating
+          } else {
+            setError("Invalid QR code. Please try again.");
+            setScanning(true);
+            setTimeout(() => {
+              setError("");
+            }, 3000); // Hide error after 3 seconds
+          }
         } else {
           requestAnimationFrame(tick);
         }
@@ -79,7 +89,7 @@ const QRScannerPage: React.FC = () => {
       <IoChevronBackCircleSharp
         onClick={() => navigate(-1)}
         className="absolute top-14 left-6 text-white text-4xl cursor-pointer z-50"
-        size={55}
+        size={60}
       />
       <div className="relative w-full h-full">
         <video
@@ -97,6 +107,11 @@ const QRScannerPage: React.FC = () => {
             }}
           ></div>
         </div>
+        {error && (
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-red-500 text-white px-4 py-2 rounded">
+            {error}
+          </div>
+        )}
       </div>
       <canvas ref={canvasRef} className="hidden"></canvas>
       <BottomNavBar />
