2a1bfead81f2134deb019386e4e3e4b5e27e618e
75098360 <lancy.gao1997@gmail.com>
2024-07-17 19:19:51 +1000

    update client not login hompage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,71 +1,38 @@
 import React from "react";
-
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 import ClientHomeComponent from "../Components/HomePageRelated/ClientHomePageRelated/ClientHomeComponent";
+import { ClientHomePageValues } from "ezpzos.core";
 
-
-export interface ClientHomePageDataProp {
-  data?: {
-    isLoggedIn:Boolean,
-    homePageButtonList:any[],
-    notificationList:any[]
-  };
-
+export interface ClientHomePageValuesProp {
+	ClientHomePageValues: {
+		IsLoggedIn: Boolean;
+		HomePageButtonList: any[];
+		NotificationList: any[];
+	};
 }
-const Home:React.FC<ClientHomePageDataProp>=({ 
-  data = {
-  isLoggedIn:false,
-  homePageButtonList:[
-  {
-    img: 'DineInIcon.png',
-    title: 'DINE IN'
-},
-{
-    img: 'BookingIcon.png',
-    title: 'BOOK'
-},
-{
-    img: 'TakeawayIcon.png',
-    title: 'TAKE AWAY'
-}
-], notificationList:[
-{
-  title: 'Vouchers',
-  content: 'You have unused vouchers!'
-},
-{
-  title: 'Vouchers',
-  content: 'You have unused vouchers!'
-}
+/**
+ * This interface defining the properties for the ClientHomePageValues
+ * @param ClientHomePageValues is an object to store mock data including Mock IsLogin boolean value, Mock HomePageButtonList and Mock NotificationList.
+ * @param IsLoggedIn is a boolean to decide if the user is logged in or not.
+ * @param HomePageButtonList is an array to store a list of mock data of Img url and button Title of homepage button,
+ * @param NotificationList is an array to store a list of mock data of notification Title and Content,
+ */
 
-]
-}
-}
-)=>{
+const Home: React.FC = ({}) => {
+	return (
+		<div>
+			<div className="flex h-screen w-screen bg-[url('./Assets/Images/MainPageBackgroundImage.png')] bg-cover relative overflow-hidden">
+				<div className="h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
+					<div className="absolute h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
+						<ClientHomeComponent ClientHomePageValues={ClientHomePageValues} />
+					</div>
+				</div>
+			</div>
 
-  return (
-    <div>
-
-
-      <div className="flex h-screen w-screen bg-[url('./Assets/Images/MainPageBackgroundImage.png')] bg-cover relative overflow-hidden">
-      <div className="h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
-        <div className="absolute h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
-
-        <ClientHomeComponent data={data} />
-
-
-
-      
-
-        </div>
-      </div>
-    </div>
-
-
-            
-
-      {/* <Footer /> */}
-    </div>
-  );
+			{/* <Footer /> */}
+		</div>
+	);
 };
 
 export default Home;
