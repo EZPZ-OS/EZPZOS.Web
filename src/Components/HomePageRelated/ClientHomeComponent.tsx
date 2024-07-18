0d1d047f50151163f4ce10079b15314215d90ed9
75098360 <lancy.gao1997@gmail.com>
2024-07-18 14:24:22 +1000

    comit 2

--- src/Components/HomePageRelated/ClientHomeComponent.tsx
+++ src/Components/HomePageRelated/ClientHomeComponent.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from "react-router-dom";
 import Logo from "../../Assets/Images/Logo.png"
 import HomePageButtons from "./HomePageButtons";
 
@@ -7,20 +8,30 @@ const ClientHomeComponent =()=>{
 
 
     return(
-
         <div>
             <div className='w-full flex flex-col justify-center items-center'>
-                <img src={Logo} className='w-[90px] h-[100px] mt-40' alt="logo"/>
-                <p className='text-2xl font-bold bg-gradient-to-b from-[#FFFFFF] to-[#FFB682F5] text-transparent bg-clip-text'>EZPZ OS</p>
-                {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text mt-7'>WELCOME BACK</p> */}
-                {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text'>:D</p> */}
+                    <img src={Logo} className='w-[90px] h-[100px] mt-44' alt="logo"/>
+                    <p className='text-2xl font-bold bg-gradient-to-b from-[#FFFFFF] to-[#FFB682F5] text-transparent bg-clip-text'>EZPZ OS</p>
+                    {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text mt-7'>WELCOME BACK</p> */}
+                    {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text'>:D</p> */}
 
-                <HomePageButtons/>
-            
-            <button className="h-[50px] w-[370px] rounded-lg mt-14 text-[#FFFFFF] text-xl bg-gradient-to-r from-[#FFB682F5] via-[#F8A27AF5] to-[#F28C83F5]">
-                SIGN IN
-            </button>
+                    <HomePageButtons/>
+
+                    <div className="flex-col">
+                        <Link to="signup" >
+                        <button className="h-[50px] w-[370px] rounded-lg mt-14 text-[#FFFFFF] text-xl bg-gradient-to-r from-[#FFB682F5] via-[#F8A27AF5] to-[#F28C83F5]">
+                         SIGN IN
+                             </button>
+                        </Link>
+                        <div className="mt-4">
+                            <span className=" text-[#dcdcdcbb] ">Don't have an account? </span>
+                            <Link to="" className="font-bold text-[#dcdcdcbb] ">Sign up :）</Link>
+                        </div>
+                    </div>
             </div>
+
+
+
         </div>
     )
 }
