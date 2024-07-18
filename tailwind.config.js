b70673371229f470381ff730e9143a42afcacbf0
lxf101 <lixinfang101@126.com>
2024-07-18 17:16:38 +1200

    develop delete-dish component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"hero-pattern": "url('./Assets/Images/MainPageBackgroundImage.png')",
-				"orange-gradient": "linear-gradient(to right, #FFB682F5, #F8A27AF5, #F28C83F5)",
-				"blue-gradient": "linear-gradient(to right, #BBDAFFF5, #FF993CF5)",
-				"brown-gradient": "linear-gradient(to right, #826D5D, #EF7221, #FC4D4C)",
-				"side-bar-gradient": "linear-gradient(to right, #c78d3f, #b85572)",
-				"nav-bar-gradient": "linear-gradient(to right, #FBA96E, #EB8787)"
-			},
-			colors: {
-				"primary-gray": "#E8E8E8",
-				"secondary-gray": "#EEEBEB"
-			},
-			fontSize: {
-				lg: ["38px"]
-			},
-			fontFamily: {
-				sans: ["Lato", "sans-serif"]
-			}
-		}
-	},
-	plugins: []
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'del-menu-bg-color': 'rgba(119, 116, 116, 0.5)'
+      },
+      backgroundImage: {
+        "hero-pattern": "url('./Assets/Images/MainPageBackgroundImage.png')",
+      },
+      fontSize: {
+        lg: ["38px"],
+      },
+      fontFamily: {
+        sans: ["Istok Web", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
 };
