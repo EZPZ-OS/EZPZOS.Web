b99a9682ceb996d51f40635ac1561275411497c5
song0305 <song0305@flinders.edu.au>
2024-07-19 22:17:38 +0930

    Implemented reusable components for Login and Signup pages

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import WelcomeBackMessage from "../../Components/LoginRelated/WelcomBackMessage";
-import ContactForm from "../../Components/LoginRelated/ContactForm";
-import Policy from "../../Components/LoginRelated/Policy";
+import WelcomeMessage from "../../Components/SignUpRelated/WelcomeMessage";
+import ContactForm from "../../Components/SignUpRelated/ContactForm";
+import Policy from "../../Components/SignUpRelated/Policy";
 
 const LoginPage: React.FC = () => {
   return (
     <div className="flex h-screen w-screen bg-hero-pattern bg-cover relative overflow-hidden">
       <div className="h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41% to-[#000000ce] to-88%">
         <div className="absolute h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41% to-[#000000ce] to-88%">
-          <WelcomeBackMessage />
+          <WelcomeMessage isLogin />
           <ContactForm isLogin />
           <Policy />
         </div>
