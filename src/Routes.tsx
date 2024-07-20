9069ab35f695a893c943f03990b0c77bfd4b6e39
LeoShang <0415150730sang@gmail.com>
2024-07-21 00:10:58 +1000

    Update Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,7 +4,7 @@ import OptPage from "./pages/OptPage/OptPage";
 import Home from "./pages/Home";
 import QRScannerPage from "./pages/QR/QRScannerPage";
 import Menu from "./pages/Menu/Menu";
-import SignupPage from "./pages/SignUp/SignupPage";
+import SignUpPage from "./pages/SignUp/SignupPage";
 import { DafaultMenuRoutesValues } from "../src/Common/Constants";
 import Profile from './pages/Profile/Profile';
 import Kitchen from './pages/Kitchen/Kitchen';
@@ -15,7 +15,7 @@ const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
       <Route path="opt" element={<OptPage />} />
-    <Route path="signup" element={<SignupPage />} />
+      <Route path="signup" element={<SignUpPage />} />
     <Route path="scan" element={<QRScannerPage />} />
     <Route
       path={DafaultMenuRoutesValues.DineInRouteDefaultValue}
