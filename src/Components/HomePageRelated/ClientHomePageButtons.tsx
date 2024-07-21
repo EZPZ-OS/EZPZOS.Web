1d5bbae237181c4f2f1bf2acd0188c086d252f11
75098360 <lancy.gao1997@gmail.com>
2024-07-21 17:13:08 +1000

    updart but still need to be improved

--- src/Components/HomePageRelated/ClientHomePageButtons.tsx
+++ src/Components/HomePageRelated/ClientHomePageButtons.tsx
@@ -7,7 +7,7 @@ console.log(props.img)
     return(
 
         <div>
-        <div className='w-full flex  justify-center items-center gap-10 mt-12'>
+        <div className='w-full flex  justify-center items-center gap-16 mt-8'>
              <div className="flex flex-col items-center gap-2 ">
               <img src={require(`../../Assets/Icons/${props.img}`)} className='w-[70px] h-[80px]
                    ' alt="logo"/>
