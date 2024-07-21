3674919266d08f85068863f5a1f96a8da38d22f9
Maryyy <111126262+danxuanw@users.noreply.github.com>
2024-07-21 23:27:33 +1000

    Add prettier (#13)
    * Add prettier
    
    * Small fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "hero-pattern": "url('./Assets/Images/MainPageBackgroundImage.png')",
-        "orange-gradient":
-          "linear-gradient(to right, #FFB682F5, #F8A27AF5, #F28C83F5)",
-        "blue-gradient": "linear-gradient(to right, #BBDAFFF5, #FF993CF5)",
-        "brown-gradient":
-          "linear-gradient(to right, #826D5D, #EF7221, #FC4D4C)",
-        "nav-bar-gradient": "linear-gradient(to right, #c78d3f, #b85572)",
-      },
-      colors: {
-        "primary-gray": "#E8E8E8",
-      },
-      fontSize: {
-        lg: ["38px"],
-      },
-      fontFamily: {
-        sans: ["Lato", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"hero-pattern": "url('./Assets/Images/MainPageBackgroundImage.png')",
+				"orange-gradient": "linear-gradient(to right, #FFB682F5, #F8A27AF5, #F28C83F5)",
+				"blue-gradient": "linear-gradient(to right, #BBDAFFF5, #FF993CF5)",
+				"brown-gradient": "linear-gradient(to right, #826D5D, #EF7221, #FC4D4C)",
+				"side-bar-gradient": "linear-gradient(to right, #c78d3f, #b85572)",
+				"nav-bar-gradient": "linear-gradient(to right, #FBA96E, #EB8787)"
+			},
+			colors: {
+				"primary-gray": "#E8E8E8",
+				"secondary-gray": "#EEEBEB"
+			},
+			fontSize: {
+				lg: ["38px"]
+			},
+			fontFamily: {
+				sans: ["Lato", "sans-serif"]
+			}
+		}
+	},
+	plugins: []
 };
