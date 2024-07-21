00b96dfb8b024cdf2ca2a7e98c120049d491f99e
75098360 <lancy.gao1997@gmail.com>
2024-07-21 17:14:22 +1000

    Reapply "updart but still need to be improved"
    This reverts commit 037eeeaab63f47f35b59bceb8bd32fd051f2a3b7.

--- src/Components/HomePageRelated/BusinessHomePageRelated/BusinessLoggedInComponent.tsx
+++ src/Components/HomePageRelated/BusinessHomePageRelated/BusinessLoggedInComponent.tsx
@@ -3,27 +3,57 @@ import Logo from "../../../Assets/Images/Logo.png"
 import BusinessHomePageButtons from "../BusinessHomePageButtons";
 
 
-const BusinessLoggedInComponent =()=>{
+import ClientHomePageButtons from "../ClientHomePageButtons";
+import BusinessAvatar from "../../../Assets/Icons/BusinessAvatar.png";
+import LogoWithName from "../../../Assets/Images/LogoWithName.png";
+import HomePageNotification from "../HomePageNotification";
+import React, {useState} from 'react';
 
+const BusinessLoggedInComponent =()=>{
+ const [notoficationList] = useState([
+        {
+            title: 'Reports',
+            content: 'You have a report!'
+        },
+        {
+            title: 'Booking notification',
+            content: 'You have 4 new bookings!'
+        },
+        {
+            title: 'Data',
+            content: 'You have 6 tables active!'
+        }
+    ])
 
 
     return(
     
     <div className="w-full flex flex-col justify-center items-center">
     
-        <img src={Logo} className='w-[90px] h-[100px] mt-44' alt="logo"/>
-        <p className='text-2xl font-bold bg-gradient-to-b from-[#FFFFFF] to-[#FFB682F5] text-transparent bg-clip-text'> Business Welcome back</p>
-        {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text mt-7'>WELCOME BACK</p> */}
-        {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text'>:D</p> */}
-
+    <img
+        src={BusinessAvatar}
+        className="w-[110px] h-[110px] mt-32"
+        alt="logo"
+      />
+<p className="text-3xl font-black	mt-8 bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text">
+        Hi @Username,
+      </p>
+      <p className="text-sm font-bold bg-gradient-to-r from-[#FBFBFB] to-[#959595] text-transparent bg-clip-text mt-1">
+        Would you like to...
+      </p>      
 <BusinessHomePageButtons/>
-        {/* <div className="flex-col">
-            <Link to="signup" >
-            <button className="h-[50px] w-[370px] rounded-lg mt-14 text-[#FFFFFF] text-xl bg-gradient-to-r from-[#FFB682F5] via-[#F8A27AF5] to-[#F28C83F5]">
-                 </button>
-            </Link>
-            
-        </div> */}
+<div className='flex flex-col items-center w-4/5 mt-6'>
+                {
+                   notoficationList.map((item, index)=>{
+                        return <HomePageNotification  key={index} title={item.title} content={item.content}/>
+                    })
+                }
+            </div>
+        <img
+          src={LogoWithName}
+          className="mt-8"
+          alt="logo"
+        />
         </div>
     )
 }
