645788fc77f96f853a2b543a25f4b5fbab5f301b
75098360 <lancy.gao1997@gmail.com>
2024-07-22 00:22:50 +1000

    tried to write front end home page buttons base
    did not success because of the <img src={props.img}  did not work

---
+++ src/Components/HomePageRelated/HomePageButtons.tsx
@@ -0,0 +1,26 @@
+
+
+
+const HomePageButtons =(props:any)=>{
+
+
+    return(
+
+        <div>
+        <div className='w-full flex justify-center items-center gap-10 mt-12'>
+             <div className="flex flex-col items-center gap-2 ">
+              <img src={props.img} className='w-[70px] h-[80px]
+                   ' alt="logo"/>
+                <div className="text-white	">{props.title}</div>
+            </div>
+
+
+        </div>
+        </div>
+    )
+}
+
+
+export default HomePageButtons;
+
+
