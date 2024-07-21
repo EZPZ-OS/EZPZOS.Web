9fdd457a41dd22dc82dcf46e0b8b35c45090d44a
danxuanw <w.dan.xuan@gmail.com>
2024-07-21 23:25:51 +1000

    Small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
-import BottomNavBar from "./Components/BottomNavBar";
 
 const App: React.FC = () => (
-  <div className="app">
-    <BrowserRouter>
-      <main className="">
-        <AppRoutes />
-        <BottomNavBar isClient={false} />
-      </main>
-    </BrowserRouter>
-    
-  </div>
+	<div className="app">
+		<BrowserRouter>
+			<main className="">
+				<AppRoutes />
+			</main>
+		</BrowserRouter>
+	</div>
 );
 
 export default App;
