3fea0cb114695d1b0a340236c9b063da3349378e
75098360 <lancy.gao1997@gmail.com>
2024-07-22 10:45:48 +1000

    update the client home page mock data
    created interface for HomePageDataProps
    pass the data={
    homepagelist:[],
    notificationlist:[]
    }
    as a React.FC<HomePageDataProp> into the client homepage component function.

--- src/Components/HomePageRelated/ClientHomePageRelated/ClientNotLoggedInComponent.tsx
+++ src/Components/HomePageRelated/ClientHomePageRelated/ClientNotLoggedInComponent.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 import Logo from "../../../Assets/Images/Logo.png"
-import ClientHomePageButtons from "../ClientHomePageButtons";
-import { iconsProp } from "./ClientLoggedInComponent";
-
-
-const ClientNotLoggedInComponent:React.FC <iconsProp>=({icons=["DineInIcon.png","TakeawayIcon.png","BookingIcon.png"
-]})=>{
+import { HomePageDataProp } from "./ClientLoggedInComponent";
+import HomePageButtons from "../HomePageButtons";
+const ClientNotLoggedInComponent:React.FC <HomePageDataProp>=({ data = {homepagelist:[
+    
+    {
+        img: 'DineInIcon.png',
+        title: 'DINE IN'
+    },
+    {
+        img: 'BookingIcon.png',
+        title: 'BOOK'
+    },
+    {
+        img: 'TakeawayIcon.png',
+        title: 'TAKE AWAY'
+    }
+]}
+})=>{
 
   
 
@@ -18,8 +30,12 @@ const ClientNotLoggedInComponent:React.FC <iconsProp>=({icons=["DineInIcon.png",
         {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text mt-7'>WELCOME BACK</p> */}
         {/* <p className='text-3xl font-bold bg-gradient-to-r from-[#CDE1FF] to-[#E56923] text-transparent bg-clip-text'>:D</p> */}
 
-        <ClientHomePageButtons {...icons=icons}/>
-
+        {/* <ClientHomePageButtons {...icons=icons}/> */}
+        <div className="flex gap-16">
+        {data.homepagelist.map((data,index)=>{
+          return <HomePageButtons key={index} img={data.img} title={data.title} />
+        })}
+        </div>
         <div className="flex-col">
             <Link to="signup" >
             <button className="h-[50px] w-[370px] rounded-lg mt-14 text-[#FFFFFF] text-xl bg-gradient-to-r from-[#FFB682F5] via-[#F8A27AF5] to-[#F28C83F5]">
@@ -30,6 +46,7 @@ const ClientNotLoggedInComponent:React.FC <iconsProp>=({icons=["DineInIcon.png",
                 <span className=" text-[#dcdcdcbb] ">Don't have an account? </span>
                 <Link to="" className="font-bold text-[#dcdcdcbb] ">Sign up :）</Link>
             </div>
+
         </div>
         </div>
     )
