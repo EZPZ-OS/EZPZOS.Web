4ba95e0dbe4c9c56452c6291d2424ea36a3740e3
LeoShang <0415150730sang@gmail.com>
2024-07-23 21:28:57 +1000

    Update ClientCartPage

--- src/Components/ClientCartComponents/ClientCartForm.tsx
+++ src/Components/ClientCartComponents/ClientCartForm.tsx
@@ -1,12 +1,15 @@
 import React,{useRef, useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
+import KungPaoChicken from "../../Assets/Images/CuisineImages/KungPaoChicken.png";
+import {AiOutlineDelete} from "react-icons/ai";
+import { IoAddOutline } from "react-icons/io5";
 interface props {
     MobileNumber?: string
 };
 
-const ClientCartForm: React.FC<props> = ({MobileNumber = "0473001475"}) => {
+const ClientCartForm: React.FC<props> = () => {
     return (
-        <div className="flex flex-wrap max-w-screen-sm font-lato w-[430px] h-[899px] bg-[#F3F3F3] bg-scroll">
+        <div className="flex flex-wrap max-w-screen-sm font-lato w-[430px] h-[899px] bg-[#F3F3F3]">
             <Link to={{pathname: `/`}}>
                 <button className="font-bold text-left w-[430px] h-[28.38px] text-[28.38px] pl-[26.07px] mt-[47px]"
                         type="button">
@@ -20,9 +23,38 @@ const ClientCartForm: React.FC<props> = ({MobileNumber = "0473001475"}) => {
             <div className="font-semibold text-left w-[430px] h-9 text-[24px] mt-4 pl-[23px]">
                 Dine In
             </div>
-            <div className="w-[430px] h-[384px] bg-scroll mt-4">
-
+            <div className="w-[430px] h-[384px] bg-scroll mt-4 overflow-y-scroll">
+                <div className="flex flex-wrap w-[384px] h-[128px] bg-scroll mt-4 ml-[22px] font-semibold border-b-[1px] border-b-[#D9D6D6]">
+                    <img src={KungPaoChicken}
+                         className="w-[70px] h-[70px] rounded-lg"/>
+                    <div className="ml-2 w-[306px] h-[70px]">
+                        <div className="w-[306px] h-[33px] text-[16px] mt-[5px] text-[16px]">
+                            Kung Pao Chicken
+                        </div>
+                        <div className="w-[306px] text-[15px] mt-[5px] text-[#4D4D4D]">
+                            $17.5
+                        </div>
+                    </div>
+                    <div className="flex justify-between w-[430px]">
+                        <button
+                            className="w-[162px] h-[34px] rounded-full bg-orange-gradient">
+                            Move to Take Away
+                        </button>
+                        <div className="flex items-center justify-evenly w-[124px] h-[38px] rounded-full bg-[#FF9900]">
+                            <button>
+                                <AiOutlineDelete/>
+                            </button>
+                            <div className="text-[14px]">
+                                1
+                            </div>
+                            <button>
+                                <IoAddOutline />
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
+            
         </div>
     );
 };
--- src/pages/ClientCart/ClientCartPage.tsx
+++ src/pages/ClientCart/ClientCartPage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import WelcomeMessage from "../../Components/SignUp/WelcomeMessage";
+import BottomNavBar from "../../Components/BottomNavBar";
 import ClientCartForm from "../../Components/ClientCartComponents/ClientCartForm";
 const ClientCartPage: React.FC = () => {
     return (
-        <div className="flex h-screen w-screen bg-hero-pattern bg-cover relative overflow-hidden">
+        <div className="flex h-screen w-screen bg-hero-pattern bg-cover relative overflow-hidden justify-center">
             <div className="h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
                 <div className="absolute h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
                     <ClientCartForm />
                 </div>
                 <hr className="w-[430px] h-[7px] "/>
+                <BottomNavBar isClient={true}/>
             </div>
         </div>
     );
