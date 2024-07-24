bf96537cbe6b77f7bbf76efa2a34913c74664b06
lxf101 <lixinfang101@126.com>
2024-07-24 16:18:58 +1200

    add react-slick component

--- src/Pages/DishProduct/DishProduct.tsx
+++ src/Pages/DishProduct/DishProduct.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
+import Slider from 'react-slick'
+
 import { FaArrowLeftLong, FaPlus } from "react-icons/fa6";
 import { GrFormSubtract } from "react-icons/gr";
 
+import demoImg01 from '../../Assets/Images/11.png'
+import demoImg02 from '../../Assets/Images/London.png'
+import demoImg03 from '../../Assets/Images/Newyork.png'
+
 const DishProduct = () => {
+
+    const settings = {
+        dots: true,
+        infinity: true,
+        speed: 500,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
     return (
         <div className="relative bg-white pb-20">
-            <div className="fixed left-5 top-5 w-full h-[38px] bg-white text-2xl flex flex-row items-center">
-                <FaArrowLeftLong fontWeight={400}/>
-                <span className="ml-2">Back</span>
+            <div className="fixed left-0 top-0 z-10 w-full h-[60px] bg-white">
+                <div className="absolute left-5 top-5 w-full h-[38px] text-2xl flex flex-row items-center">
+                    <FaArrowLeftLong fontWeight={400}/>
+                    <span className="ml-2">Back</span>
+                </div>
             </div>
             <div className="h-screen ml-5 mr-5 mt-[68px]">
                 {/* 轮播图部分 */}
-                <div className="w-full h-56 bg-red-300"></div>
+                <div className="w-full h-56">
+                    <Slider {...settings} className="w-full h-full">
+                        <div className="w-full h-56">
+                            <img src={demoImg01} className="w-full h-full"/>
+                        </div>
+                        <div className="w-full h-56">
+                            <img src={demoImg02} className="w-full h-full"/>
+                        </div>
+                        <div className="w-full h-56">
+                            <img src={demoImg03} className="w-full h-full"/>
+                        </div>
+                    </Slider>
+                </div>
                 <div className="text-[24px] font-bold mt-4 text-text-dishpro-color">Seafood Fried noodles</div>
                 <ul className="mt-2 flex flex-row">
                     <li className="h-5 leading-5 text-sm text-[#5A4F4F] px-2 bg-gradient-to-r from-[#F279A5] to-[#FA986E] opacity-90 rounded-sm mr-1">gluten free</li>
@@ -43,12 +73,12 @@ const DishProduct = () => {
                 </div>
                 <div className='h-[3px] bg-[#E2DEDE] mt-5'></div>
                 <div className="w-10/12 mt-3 mx-auto flex flex-row justify-between items-center h-8">
-                    <span className="w-2/4 h-full">$22.0</span>
+                    <span className="w-2/4 h-full text-text-dishpro-color font-bold text-[20px]">$22.0</span>
                     <div className="w-2/4 h-full flex flex-row items-center justify-around">
                         <span className="w-8 h-8 leading-8 flex items-center justify-center rounded-circle bg-gradient-to-b from-[#FF993C] to-[#DC4200] opacity-90">
                             <GrFormSubtract style={{'color': '#fff'}}/>
                         </span>
-                        <span className="flex-1 text-center">1</span>
+                        <span className="flex-1 text-center text-text-dishpro-color font-bold text-[20px]">1</span>
                         <span className="w-8 h-8 leading-8 flex items-center justify-center rounded-circle bg-gradient-to-b from-[#FF993C] to-[#DC4200] opacity-90">
                             <FaPlus style={{'color': '#fff'}}/>
                         </span>
