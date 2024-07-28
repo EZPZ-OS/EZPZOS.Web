071ef0bb2c70b9476116c49bc784fb6842f32b37
Maryyy <111126262+danxuanw@users.noreply.github.com>
2024-07-28 21:12:19 +1000

    Add feature client restaurant contact detail page (#20)
    1 Done UI
    2Done menu tab function, switch between dine-in & take away
    3Done notification
    4Change Routes, remove /menu; add in /menu-dine-in ; /menu-takeaway
    5convert most useful colour codes into config
    6added comments
    7centralised constants into one .ts
    8business contact card
    9business contact detials
    10googlemap
    11tag list

--- src/Components/BottomNavBar.tsx
+++ src/Components/BottomNavBar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 // import { RiHome2Line } from "react-icons/ri"; // discarded home icon
 // import { TbClipboardText } from "react-icons/tb"; // discarded menu icon
 // import { PiHandbag } from "react-icons/pi"; // discarded take away icon
-
-import HomeIcon from "../Assets/Icons/BottomNavBarHomeIcon.png" // home
-import { FaDove, FaUser } from "react-icons/fa6"; //  profile 
+import HomeIcon from "../Assets/Icons/BottomNavBarHomeIcon.png"; // home
+import { FaDove, FaUser } from "react-icons/fa6"; //  profile
 import { PiForkKnifeBold } from "react-icons/pi"; // kitchen icon
-import MenuIcon from '../Assets/Icons/BottomNavBarMenuIcon.png'; // menu icon
+import MenuIcon from "../Assets/Icons/BottomNavBarMenuIcon.png"; // menu icon
 import TakeAwayIcon from "../Assets/Icons/BottomNavBarTakeAwayIcon.png"; // take away icon
 
 /*
@@ -29,56 +28,60 @@ interface BottomNavBarProps {
   isClient: boolean;
 }
 
-const BottomNavBar: React.FC<BottomNavBarProps> = (
-  { isClient }) => {
-
+const BottomNavBar: React.FC<BottomNavBarProps> = ({ isClient }) => {
   const businessNavBar = {
     iconList: [HomeIcon, <PiForkKnifeBold />, MenuIcon, <FaUser />],
-    wordList: ['HOME', 'KITCHEN', 'MENU', 'PROFILE'],
-    pathList: ['/', 'kitchen', 'menu', 'profile']
-  }
+    wordList: ["HOME", "KITCHEN", "MENU", "PROFILE"],
+    pathList: ["/", "/kitchen", "/menu", "/profile"],
+  };
 
   const clientNavBar = {
     iconList: [HomeIcon, MenuIcon, TakeAwayIcon, <FaUser />],
-    wordList: ['Home', 'Menu', 'Take away', 'Profile'],
-    pathList: ['/', 'menu', 'takeaway', 'profile']
-  }
+    wordList: ["Home", "Menu", "Take away", "Profile"],
+    pathList: ["/", "/menu-dinein", "/menu-takeaway", "/profile"],
+  };
 
-  const navBar = isClient ? clientNavBar : businessNavBar
+  const navBar = isClient ? clientNavBar : businessNavBar;
   const navigate = useNavigate();
 
   // The key function to render the icon.
-  const renderIcon = (icon:any, alt:string, className:string) =>{
-    if (typeof icon === 'string'){ 
+  const renderIcon = (icon: any, alt: string, className: string) => {
+    if (typeof icon === "string") {
       // img icon
       return <img src={icon} alt={alt} className={className} />;
-    } else{
+    } else {
       // react icon component
       return React.cloneElement(icon, { className });
     }
-  }
-
-
+  };
 
   // console.log('here i am')
   return (
     <div className="bg-[#D9D9D9] pt-[8px] text-[#988B8B] w-full h-[88px] flex bottom-0 text-center fixed justify-center px-[10%] gap-0">
       <div className=" w-full h-[51px] flex justify-between padding-0">
         {navBar.iconList.map((icon, index) => (
-          <div key={index} className="flex flex-col items-center cursor-pointer" onClick={() => navigate(navBar.pathList[index])}>
+          <div
+            key={index}
+            className="flex flex-col items-center cursor-pointer"
+            onClick={() => navigate(navBar.pathList[index])}
+          >
             {/* here are two divs for each page: icon div + text span div. In the icon div, the flex items-center are to centralize the icon */}
-            <div className="h-[30px] flex items-center">  
+            <div className="h-[30px] flex items-center">
               {/* Invoke the renderIcon function*/}
-              {renderIcon(icon, navBar.wordList[index], 'h-full w-full object-contain')}
-              </div>
-            <span className="font-istok text-[14px] font-thin leading-[21px] text-center">{navBar.wordList[index]}</span>
+              {renderIcon(
+                icon,
+                navBar.wordList[index],
+                "h-full w-full object-contain"
+              )}
+            </div>
+            <span className="font-istok text-[14px] font-thin leading-[21px] text-center">
+              {navBar.wordList[index]}
+            </span>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default BottomNavBar
-
-
+export default BottomNavBar;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,31 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import QRScannerPage from "./pages/QR/QRScannerPage";
 import Menu from "./pages/Menu/Menu";
-import { DafaultMenuRoutesValues } from "../src/Common/Constants";
+import { DafaultMenuRoutesValues } from "ezpzos.core";
 import Profile from "./pages/Profile/Profile";
 import Kitchen from "./pages/Kitchen/Kitchen";
 import LoginPage from "./pages/Login/LoginPage";
 import SignUpPage from "./pages/SignUp/SignupPage";
 
 const AppRoutes: React.FC = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="signup" element={<SignUpPage />} />
-    <Route path="scan" element={<QRScannerPage />} />
-    <Route
-      path={DafaultMenuRoutesValues.DineInRouteDefaultValue}
-      element={<Menu />}
-    />
-    <Route
-      path={DafaultMenuRoutesValues.TakeAwayRouteDefaultValue}
-      element={<Menu />}
-    />
-    <Route path="profile" element={<Profile />} />
-    <Route path="kitchen" element={<Kitchen />} />
+	<Routes>
+		{/* Client Routes */}
+		<Route path="/" element={<Home />} />
+		<Route path="signup" element={<SignUpPage />} />
+		<Route path="scan" element={<QRScannerPage />} />
+		<Route path={DafaultMenuRoutesValues.DineInRouteDefaultValue} element={<Menu />} />
+		<Route path={DafaultMenuRoutesValues.TakeAwayRouteDefaultValue} element={<Menu />} />
+		<Route path="profile" element={<Profile />} />
 
-    <Route path="menu" element={<Menu />} />
-    <Route path="login" element={<LoginPage />} />
-  </Routes>
+		{/* Business Routes */}
+		<Route path="kitchen" element={<Kitchen />} />
+	</Routes>
 );
 
 export default AppRoutes;
