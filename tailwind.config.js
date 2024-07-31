20e4bbf6deb5c220729269217adc65d04483dd17
LeoShang <0415150730sang@gmail.com>
2024-07-31 10:07:10 +1000

    Update tailwind.config.js to match main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "hero-pattern": "url('./Assets/Images/MainPageBackgroundImage.png')",
-        'gradient-border': 'linear-gradient(to right, #ED897F 40%, #FF9900 100%)',
-        "orange-gradient":
-          "linear-gradient(to right, #FFB682F5, #F8A27AF5, #F28C83F5)",
-        "blue-gradient": "linear-gradient(to right, #BBDAFFF5, #FF993CF5)",
-        "brown-gradient":
-          "linear-gradient(to right, #826D5D, #EF7221, #FC4D4C)",
-        "nav-bar-gradient": "linear-gradient(to right, #c78d3f, #b85572)",
-      },
-      colors: {
-        "primary-gray": "#E8E8E8",
-      },
-      fontSize: {
-        lg: ["38px"],
-      },
-      fontFamily: {
-        sans: ["Istok Web", "sans-serif"],
-        lato: ['Lato', 'sans-serif'],
-        sans: ["Lato", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
 	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 	theme: {
 		extend: {
