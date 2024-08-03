4d1d21d92c7dc72fe2f5c69474c5343832a773ee
CFong66 <chrisfang11@gmail.com>
2024-08-03 20:29:01 +1000

    update ec2 config sh file

--- ec2_1st.sh
+++ ec2_1st.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Update and install packages
-sudo yum update -y
-sudo yum install -y nodejs npm git docker
-
 # generate the ssh key
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
-
+sudo yum update -y
+sudo yum install -y nodejs npm git docker
