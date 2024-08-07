065bcb18ea4ea4912759b85514386c8f5e9158cd
75098360 <64082644+75098360@users.noreply.github.com>
2024-08-07 13:21:42 +1000

    UPDATE-feature-combine-login-and-signup-page (#22)
    * combine client side login and signup page
    
    * combine the login and signup page
    
    1. combine login and signup page together.
    2. change the route for LoginSignupPage
    3. change the page directory file name and component directory file name for LoginSignupPage
    
    * update a close button on the top right corner
    
    1. add a close button
    2. link the route to the home page
    
    * constanlise the strings and save it in EZPZ.Core
    
    * constantlize the hard coding string, add comment to explain the components and props
    
    * change the comment structure to @param
    
    * format the pages
    
    * commit try to merge into main

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,14 +6,17 @@ import Menu from "./pages/Menu/Menu";
 import { DafaultMenuRoutesValues } from "ezpzos.core";
 import Profile from "./pages/Profile/Profile";
 import Kitchen from "./pages/Kitchen/Kitchen";
-import SignUpPage from "./pages/SignUpFix/SignUpPageFix";
+import LoginSignupPage from "./Pages/LoginOrSignup/LoginOrSignup";
 import OTPPage from "./pages/OTPPage/OTPPage";
+
 
 const AppRoutes: React.FC = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
-		<Route path="signup" element={<SignUpPage />} />
+		<Route path="signup" element={<LoginSignupPage  isLogin={false}  />} />
+		<Route path="login" element={<LoginSignupPage isLogin={true} />} />
 		<Route path="otp" element={<OTPPage />} />
+
 		<Route path="scan" element={<QRScannerPage />} />
 		<Route path={DafaultMenuRoutesValues.DineInRouteDefaultValue} element={<Menu />} />
 		<Route path={DafaultMenuRoutesValues.TakeAwayRouteDefaultValue} element={<Menu />} />
