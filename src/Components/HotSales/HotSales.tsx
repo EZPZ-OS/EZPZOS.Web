cb31c8042cca6cc925761bb34d609b1b25feba37
75098360 <lancy.gao1997@gmail.com>
2024-08-09 13:42:22 +1000

    add use ref and use effect, fix bug

--- src/Components/HotSales/HotSales.tsx
+++ src/Components/HotSales/HotSales.tsx
@@ -1,146 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import HotSalesCard from "./HotSalesCard";
-import { FaChevronRight } from "react-icons/fa";
-import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+
 /**
  * This is the component of the hotsales, with child component HotSalesCard.
  * Card picture is imported from Images, may need modification later.
- * Currently , the maximum number of cards is 6 ,which means  at most 6 cards can be added in the hotsales.
- * The function of right button is done and left button is also added.
- * When the card is moved to rightmost/leftmost side, the right/left button is invisible.
+ * Currently, the maximum number of cards is 6, which means at most 6 cards can be added in the hotsales.
+ * The function of the right button is done and the left button is also added.
+ * When the card is moved to the rightmost/leftmost side, the right/left button is invisible.
  * @returns
  */
-
 export default function HotSales() {
-	const [currentLocation, setCurrentLocation] = useState(0);
-	const cardsMoveToogle = () => {
-		if (currentLocation === 0) {
-			setCurrentLocation(currentLocation + 480);
-		} else {
-			setCurrentLocation(0);
+	const [showLeft, setShowLeft] = useState(false);
+	const [showRight, setShowRight] = useState(true);
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const container = containerRef.current;
+			if (container) {
+				const scrollLeft = container.scrollLeft;
+				const scrollWidth = container.scrollWidth;
+				const clientWidth = container.clientWidth;
+
+				setShowLeft(scrollLeft > 0);
+				setShowRight(scrollLeft < scrollWidth - clientWidth);
+			}
+		};
+
+		handleScroll(); // Initial check
+
+		const container  = containerRef.current;
+		if (container) {
+			container.addEventListener("scroll", handleScroll);
+			return () => container.removeEventListener("scroll", handleScroll);
+		}
+	}, []);
+
+	const scrollContainer = (amount: number) => {
+		const container = containerRef.current;
+		if (container) {
+			container.scrollBy({
+				left: amount,
+				behavior: "smooth"
+			});
 		}
 	};
+
 	return (
 		<div className="hotSales_text h-[270px] bg-gradient-to-b from-[#FBA96E] to-[#FFCECE78] relative">
-			<div className='absolute size-[46px] top-[110px] right-[14px] z-50 bg-[#FFF3F3] rounded-full flex justify-center items-center ' style={{display:currentLocation!==0?'none':'flex'}}  onClick={cardsMoveToogle}>
-          <FaChevronRight className="gradient-icon text-[#d17461] text-[30px]"  />
-        </div>
-        <div className='absolute size-[46px] top-[110px] left-[14px] z-50 bg-[#FFF3F3] rounded-full flex justify-center items-center ' style={{display:currentLocation!==0?'flex':'none'}}  onClick={cardsMoveToogle}>
-          <FaChevronLeft className="gradient-icon text-[#d17461] text-[30px]"  />
-        </div>
-        <span className="font-bold text-[20px]/[30px] ml-[17px]">Hot Sale</span>
-        <span className="text-[14px]/[21px] text-[#4D4D4D] ml-[17px]">80% customer has ordered</span>
-        <div className="pl-[10px] mt-[15px] flex w-[1000px] transform transition-transform duration-1000 ]" style={{ transform: `translateX(-${currentLocation}px)` }}>
-        <HotSalesCard rank={1} dishName="Stewed beef with potato" like_pc={98} like_qty={1002} price={17.2}/>
-        <HotSalesCard rank={2} dishName="Stewed beef with pasta" like_pc={95} like_qty={992} price={25}/>
-        <HotSalesCard rank={3} dishName="Stewed beef with pasta" like_pc={95} like_qty={992} price={25}/>
-        <HotSalesCard rank={4} dishName="Stewed beef with pasta" like_pc={95} like_qty={992} price={25}/>
-        <HotSalesCard rank={5} dishName="Stewed beef with pasta" like_pc={95} like_qty={992} price={25}/>
-        <HotSalesCard rank={6} dishName="Stewed beef with pasta" like_pc={95} like_qty={992} price={25}/>
-        
-        </div>
-
-			{/* <div className="flex flex-col m-auto p-auto">
-				<div
-					className="absolute size-[46px] top-[110px] right-[14px] z-50 bg-[#FFF3F3] rounded-full flex justify-center items-center "
-					style={{ display: currentLocation !== 0 ? "none" : "flex" }}
-					onClick={cardsMoveToogle}
-				>
-					<FaChevronRight className="gradient-icon text-[#d17461] text-[30px]" />
-				</div>
-				<div
-					className="absolute size-[46px] top-[110px] left-[14px] z-50 bg-[#FFF3F3] rounded-full flex justify-center items-center "
-					style={{ display: currentLocation !== 0 ? "flex" : "none" }}
-					onClick={cardsMoveToogle}
-				>
-					<FaChevronLeft className="gradient-icon text-[#d17461] text-[30px]" />
-				</div>
-				<div className="flex overflow-x-scroll pb-10 no-scrollbar hide-scroll-bar">
-					<div className="flex flex-nowrap mt-10 lg:ml-40 md:ml-10 ml-2 mr-6">
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
+			
+			<div className="relative flex flex-col m-auto p-auto">
+			<div className="mt-2">
+			        <span className="font-bold text-[20px]/[30px] ml-[17px] ">Hot Sale</span>
+					<span className="text-[14px]/[21px] text-[#4D4D4D] ml-[17px]">80% customer has ordered</span>
+					</div>
+				{showLeft && (
+					<button
+						onClick={() => scrollContainer(-containerRef.current?.clientWidth * 2)}
+						className="absolute left-0 ml-2 p-2 text-gray-600 hover:text-gray-900 z-10"
+					>
+						<div className="relative size-[46px] top-[100px] l-[14px] z-50 bg-[#FFF3F3] rounded-full flex justify-center items-center">
+							<FaChevronLeft className="gradient-icon text-[#d17461] text-[30px] " size={24} />
 						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
+					</button>
+				)}
+				{showRight && (
+					<button
+						onClick={() => scrollContainer(containerRef.current?.clientWidth * 2)}
+						className="absolute right-0 mr-2 p-2 text-gray-600 hover:text-gray-900 z-10"
+					>
+						<div className="relative size-[46px] top-[100px] r-[50px] z-50 bg-[#FFF3F3] rounded-full flex justify-center items-center">
+							<FaChevronRight className="gradient-icon text-[#d17461] text-[30px] " size={24} />
 						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
-						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
-						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
-						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
-						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
-						</div>
-						<div className="inline-block px-1">
-							<HotSalesCard
-								rank={1}
-								dishName="Stewed beef with potato"
-								like_pc={98}
-								like_qty={1002}
-								price={17.2}
-							/>
-						</div>
+					</button>
+				)}
+				<div ref={containerRef} className="flex overflow-x-scroll pb-10 no-scrollbar hide-scroll-bar">
+					<div className="flex flex-nowrap mt-0 lg:ml-40 md:ml-10 ml-2 mr-2 transform transition-transform duration-1000">
+						{[...Array(8)].map((_, index) => (
+							<div key={index} className="inline-block px-1">
+								<HotSalesCard
+									rank={index + 1}
+									dishName="Stewed beef with potato"
+									like_pc={98}
+									like_qty={1002}
+									price={17.2}
+								/>
+							</div>
+						))}
 					</div>
 				</div>
-			</div> */}
-			<style>
-				{/* .hide-scroll-bar {
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
-.hide-scroll-bar::-webkit-scrollbar {
-  display: none;
-} */}
-			</style>
+			</div>
 		</div>
 	);
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HotSales from "../Components/HotSales/HotSales";
+
+const Home = () => {
+  return <div><HotSales/></div>;
+};
+
+export default Home;
