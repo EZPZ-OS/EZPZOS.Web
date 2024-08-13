a19eeaea7f1beff6b14015cdffa0837e769c7610
Ke <ningke0914@163.com>
2024-08-14 09:53:54 +1000

    Centralize the  '+' pattern in button. ======= Warning: This branch is inherited and hard copied directly from branch {ADD-feature-client-menu-list-and-horizontal-category-bar} , where  we applied as a workaround to deal with config chaos when merging from the main.  The old git history will be kept on the old branch just in case. ======= TODO: 1. Change the project layout of core menu component. 2. Documentation & PR

--- src/Components/MenuList/CoreMenu/AddButton.tsx
+++ src/Components/MenuList/CoreMenu/AddButton.tsx
@@ -21,7 +21,7 @@ export const AddButton : React.FC<pathProps> = ({dishDetailPath})=>{
         <button className='relative flex items-center justify-center w-[33px] h-[33px] bg-gradient-to-r from-orange-500 to-red-600 rounded-full'
             // ! This is just a placeholder as the hooker of dish detail page.
             onClick={()=>navigate(dishDetailPath)}>
-            <span className='absolute top-[-5px] text-white text-4xl w-[33px] h-[33px]'> + </span> 
+            <span className='absolute top-[-0.5px] text-white text-4xl w-[33px] h-[33px]'> + </span> 
         </button>
     )
 }
