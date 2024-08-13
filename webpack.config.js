b6fa44c92bd1273e2788a67c9474071044dcbce5
Harrison Ning <ningke0914@163.com>
2024-08-14 01:01:18 +1000

    hard copy

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
 	],
 	resolve: {
 		modules: [__dirname, "src", "node_modules"],
-		extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+		extensions: [".*", ".js", ".jsx", ".tsx", ".ts"],
 		fallback: {
 			url: require.resolve("url"),
 			fs: require.resolve("graceful-fs"),
