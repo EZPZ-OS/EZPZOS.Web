df21ff773a2fd42f01d5b8fdec527e4b44b30fd7
75098360 <lancy.gao1997@gmail.com>
2024-08-15 17:02:30 +1000

    move the mock data into core

--- src/Pages/PastOrder/PastOrder.tsx
+++ src/Pages/PastOrder/PastOrder.tsx
@@ -1,59 +1,61 @@
-import React, {useState} from 'react'
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { DefaultPastOrderContent } from "ezpzos.core";
+import { DefaultPastOrderContent, PastOrderList } from "ezpzos.core";
+import PastOrderComponent from "../../Components/UserProfileRelated/PastOrderComponent";
+import { Link } from "react-router-dom";
 
-import SinglePastOrder from '../../Components/SinglePastOrder/SinglePastOrder'
+export interface PastOrderProps {
+	OrderId: string;
+	Title: string;
+	ItemNum: number;
+	Price: number;
+	Date: string;
+	Status: string;
+}
+/**
+ * This interface defining the properties for the PastOrderProps
+ * @param OrderId pass a order string type id. 
+ * @param Title pass a string type title of the pastorder restaurant.
+ * @param ItemNum pass a number to show how many items in the past order.
+ * @param Price pass a total number count of the past order price.
+ * @param Date pass string type date of the past order.
+ * @param Status pass a Status of the past order.
+
+ * 
+ */
 
 const PastOrder = () => {
-    const [oldOrder, setOldOrder] = useState([
-        {
-            "orderId": "1",
-            "title": "DemoData sichuan Cuisine",
-            "itemNum": 5,
-            "price": "27.87",
-            "date": "2024-06-27",
-            "status": "Completed"
-        },
-        {
-            "orderId": "2",
-            "title": "Hot pot",
-            "itemNum": 6,
-            "price": "37.87",
-            "date": "2024-06-28",
-            "status": "Completed"
-        }
-    ])
+	// State to track the currently selected order for re-ordering
+	return (
+		<div className="relative w-full h-screen bg-[#CBC2C2] bg-opacity-35">
+			<div className="absolute w-full h-[calc(100vh_-_56px)] bg-white top-[56px] left-0">
+				<div className="relative h-[70px]">
+					<div className="absolute left-5 top-5 h-[38px] text-2xl flex flex-row items-center">
+						<FaArrowLeftLong fontWeight={400} />
+						<Link to="/">
+							<span className="ml-2">{DefaultPastOrderContent.Back}</span>
+						</Link>
+					</div>
+					<div className="absolute left-0 right-0 top-0 bottom-0 m-auto overflow-hidden text-[#574545] text-xl w-[140px] h-[22px] text-center">
+						{DefaultPastOrderContent.Title}
+					</div>
+				</div>
+				<p className="text-sm text-[#574545] font-bold ml-5">{DefaultPastOrderContent.More}</p>
 
-    function goBack(){
+				{/* This map is used to map the pastorderlist item into the past order component */}
+				{PastOrderList.map(item => (
+					<PastOrderComponent
+						key={item.OrderId}
+						OrderId={item.OrderId}
+						Title={item.Title}
+						ItemNum={item.ItemNum}
+						Price={item.Price}
+						Date={item.Date}
+						Status={item.Status}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
 
-    }
-
-    function handleReOrder(orderId: string){
-        // request api -> order again
-    }
-
-
-    return (
-        <div className="relative w-full h-screen bg-[#CBC2C2] bg-opacity-35">
-            <div className="absolute w-full h-[calc(100vh_-_56px)] bg-white top-[56px] left-0">
-                <div className="relative h-[70px]">
-                    <div className="absolute left-5 top-5 h-[38px] text-2xl flex flex-row items-center">
-                        <FaArrowLeftLong fontWeight={400}/>
-                        <span className="ml-2" onClick={goBack}>{DefaultPastOrderContent.Back}</span>
-                    </div>
-                    <div className="absolute left-0 right-0 top-0 bottom-0 m-auto overflow-hidden color-[#574545] text-xl w-[140px] h-[22px] text-center">{DefaultPastOrderContent.Title}</div>
-                </div>
-                <p className="text-sm color-[#574545] font-bold ml-5">{DefaultPastOrderContent.More}</p>
-                {
-                    oldOrder.map((item, index) => {
-                        return (
-                            <SinglePastOrder key={item.orderId} orderItem={item} handleReOrder={handleReOrder}/>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-} 
-
-export default PastOrder
+export default PastOrder;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,36 @@
 import React from "react";
-import HotSales from "../Components/HotSales/HotSales";
+import ClientHomeComponent from "../Components/HomePageRelated/ClientHomePageRelated/ClientHomeComponent";
+import { ClientHomePageValues } from "ezpzos.core";
 
-const Home = () => {
-  return <div><HotSales/></div>;
+export interface ClientHomePageValuesProp {
+	ClientHomePageValues: {
+		IsLoggedIn: Boolean;
+		HomePageButtonList: any[];
+		NotificationList: any[];
+	};
+}
+/**
+ * This interface defining the properties for the ClientHomePageValues
+ * @param ClientHomePageValues is an object to store mock data including Mock IsLogin boolean value, Mock HomePageButtonList and Mock NotificationList.
+ * @param IsLoggedIn is a boolean to decide if the user is logged in or not.
+ * @param HomePageButtonList is an array to store a list of mock data of Img url and button Title of homepage button,
+ * @param NotificationList is an array to store a list of mock data of notification Title and Content,
+ */
+
+const Home: React.FC = ({}) => {
+	return (
+		<div>
+			<div className="flex h-screen w-screen bg-[url('./Assets/Images/MainPageBackgroundImage.png')] bg-cover relative overflow-hidden">
+				<div className="h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
+					<div className="absolute h-screen w-screen bg-gradient-to-tl from-transparent from-0% via-[#33291f88] via-41%  to-[#000000ce] to-88%">
+						<ClientHomeComponent ClientHomePageValues={ClientHomePageValues} />
+					</div>
+				</div>
+			</div>
+
+			{/* <Footer /> */}
+		</div>
+	);
 };
 
 export default Home;
