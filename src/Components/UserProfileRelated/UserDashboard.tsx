dc4245b48158523dcc5dbaede00613efc1d7171e
Linfei Fu <linfei.fu.2803@gmail.com>
2024-08-27 20:04:31 +1000

    created Profile page and UserDashboard component

---
+++ src/Components/UserProfileRelated/UserDashboard.tsx
@@ -0,0 +1,14 @@
+interface UserDashboardProps {
+    avatar: string | null;
+    username: string | null;
+}
+
+const UserDashboard:React.FC<UserDashboardProps>= (avatar, username) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserDashboard;
--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,13 +1,53 @@
 import React from "react";
+import jwtDecode from 'jwt-decode';
 import TopNav from "../../Components/TopNav";
 import BottomNavBar from "../../Components/BottomNavBar";
+import UserDashboard from "../../Components/UserProfileRelated/UserDashboard";
+import { useNavigate } from 'react-router-dom';
 
-// This it fake substitution page for profile. Used only in bottom nav bar.
+interface User {
+    avatar: string;
+    username: string;
+    mobile: string;
+    email: string;
+}
+interface DecodedToken {
+    Avatar: string;
+    Username: string;
+    Mobile: string;
+    Email: string;
+}
+
+const getUserFromToken = (): User | null => {
+    const token = localStorage.getItem('authToken');
+    if (!token) return null;
+
+    try {
+        const decodedToken: any = jwtDecode(token);
+        const user: User = {
+            avatar: decodedToken.Avatar,
+            username: decodedToken.Username,
+            mobile: decodedToken.Mobile,
+            email: decodedToken.Email
+        };
+        return user;
+    } catch (error) {
+        console.error('Error decoding token:', error);
+        return null;
+    }
+};
 const Profile: React.FC = () => {
+    const nagivate = useNavigate();
+    const user = getUserFromToken();
+    if (!user) {
+        // Handle the case where user is null, e.g., redirect to login or show a message
+        return <div>User not found. Please log in again.</div>;
+    }
     return(
         <div className="flex flex-col items-center">
-            <TopNav />
-            <BottomNavBar isClient/>
+            <TopNav hideSearch={true}/> {/* Pass hideSearch prop to hide the search icon */}
+            <UserDashboard avatar = {user.avatar} username = {user.username}/>
+            <BottomNavBar isClient={true}/>
         </div>
     )
 }
