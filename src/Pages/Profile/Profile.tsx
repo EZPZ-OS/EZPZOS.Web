4c2e0c6275366200abda738c2642bce20d2d318b
LinfeiFu <linfei.fu.2803@gmail.com>
2024-09-04 22:52:56 +1000

    Add feature user profile (#35)
    * created Profile page and UserDashboard component
    
    * added comments
    
    * created authChecker and validateToken service
    
    * updated comments
    
    * deleted verifyToken service and authChecker
    
    * fixed error handeling in the response interceptor
    
    * combined authService and otpService into PubliceService

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopNav from "../../Components/TopNav";
 import BottomNavBar from "../../Components/BottomNavBar";
+import UserDashboard from "../../Components/UserProfileRelated/UserDashboard";
+import { useSelector } from "react-redux";
+import { RootState } from "../../Store/Store";
+import { useNavigate } from "react-router-dom";
+import AlertTag from "../../Components/AlertTag";
 
-// This it fake substitution page for profile. Used only in bottom nav bar.
+/**
+ * This is the client profile page.
+ * @param user is user information received from database when user logged in to be retrieved from Redux for display purpose.
+ */
+
 const Profile: React.FC = () => {
+    let user = useSelector((state: RootState) => state.auth.user);
+    const navigate = useNavigate();
+    const [showAlert, setShowAlert] = useState(false);
+
+    useEffect(() => {
+        if (!user) {
+            setShowAlert(true);
+
+            const timer = setTimeout(() => {
+                navigate("/");
+            }, 3000);
+
+            // Cleanup the timeout if the component unmounts before the timeout triggers
+            return () => clearTimeout(timer);
+        }
+    }, [user, navigate]);
+
+    if (!user) {
+        return (
+            <div className="flex flex-col items-center">
+                {showAlert && <AlertTag alertMessage="User not logged in" isError={true} />}
+            </div>
+        );
+    }
     return(
         <div className="flex flex-col items-center">
-            <TopNav />
-            <BottomNavBar isClient/>
+            <TopNav hideSearch={true}/> {/* Pass hideSearch prop to hide the search icon */}
+            <UserDashboard avatar = {user.Avatar} username = {user.Username}/>
+            <BottomNavBar isClient={true}/>
         </div>
     )
 }
