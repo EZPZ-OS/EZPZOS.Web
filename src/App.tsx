e36c83c324fe425a6975ee9bb001306b9230b392
Linfei Fu <linfei.fu.2803@gmail.com>
2024-09-04 20:38:18 +1000

    deleted verifyToken service and authChecker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
-import { AuthChecker } from "./Services/AuthService";
 
 const App: React.FC = () => (
 	<div className="app">
 		<BrowserRouter>
-			{/* Ensure authToken is checked before rendering routes */}
-			<AuthChecker />
 			<main className="">
 				<AppRoutes />
 			</main>
--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,9 +1,5 @@
 import apiClient from "../Utils/axiosConfig";
 import { DefaultLoginSignupValues, LogHandler, LogLevel, OTPType, User } from "ezpzos.core";
-import { store } from "../Store/Store";
-import { login, logout } from "../Store/AuthSlice";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 const logger = new LogHandler("AuthService");
 
@@ -85,44 +81,4 @@ export const AuthService = {
 			return { success: false, message: errorMessage };
 		}
 	},
-	// Validate token
-	validateToken: async (): Promise<boolean> => {
-		try {
-			const authToken = localStorage.getItem("authToken");
-			const savedUser = localStorage.getItem("user");
-
-			if (!authToken || !savedUser) {
-				store.dispatch(logout());
-				return false;
-			}
-
-			// Validate the token by making a request to the backend
-			await apiClient.get("/auth/validate-token", {
-				headers: {
-					Authorization: `Bearer ${authToken}`
-				}
-			});
-
-			// If successful, dispatch the login action with the existing token and user data
-			const user: User = JSON.parse(savedUser);
-			store.dispatch(login({ token: authToken, user }));
-
-			return true;
-		} catch (error: any) {
-			// If token is invalid or expired, log the user out
-			store.dispatch(logout());
-			return false;
-		}
-	}
-};
-
-// AuthChecker component
-export const AuthChecker = () => {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        AuthService.validateToken();
-    }, [dispatch]);
-
-    return null;
-};
+}
