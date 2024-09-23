dafe98ff9aab7be8047ebdcb67b04c7f72f6293f
danxuanw <w.dan.xuan@gmail.com>
2024-09-23 14:48:45 +1000

    PR #33 ready to merge

--- src/pages/ClientCart/ClientCartPage.tsx
+++ src/pages/ClientCart/ClientCartPage.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import BottomNavBar from "../../Components/BottomNavBar";
 import ClientCartForm from "../../Components/ClientCartComponents/ClientCartForm";
-import WelcomeMessage from "../../Components/SignUp/WelcomeMessage";
-import Policy from "../../Components/SignUp/Policy";
 const ClientCartPage: React.FC = () => {
-    return (
-        <div className="flex justify-center h-screen w-screen bg-hero-pattern bg-cover relative bg-scroll ">
-            <ClientCartForm />
-        </div>
-    );
+	return (
+		<div className="flex justify-center h-screen w-screen bg-hero-pattern bg-cover relative bg-scroll ">
+			<ClientCartForm />
+		</div>
+	);
 };
 
 export default ClientCartPage;
